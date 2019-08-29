d651921c1361fbf9e6e05973f726d8f481352aab
Sagar Dabhi <sagar.dabhi@xongolab.com>
2019-08-29 18:14:59 +0530

    first commit

---
+++ SDConverter/SDConverter.h
@@ -0,0 +1,19 @@
+//
+//  SDConverter.h
+//  SDConverter
+//
+//  Created by Xongolab Shree on 29/08/19.
+//  Copyright © 2019 Xongolab Shree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDConverter.
+FOUNDATION_EXPORT double SDConverterVersionNumber;
+
+//! Project version string for SDConverter.
+FOUNDATION_EXPORT const unsigned char SDConverterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDConverter/PublicHeader.h>
+
+
---
+++ SDConverterExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDConverterExample
+//
+//  Created by Xongolab Shree on 29/08/19.
+//  Copyright © 2019 Xongolab Shree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
