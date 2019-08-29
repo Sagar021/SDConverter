09914521db5deab8b3c380ee42e4051f903bcc7d
Sagar Dabhi <sagar.dabhi@xongolab.com>
2019-08-29 18:37:38 +0530

    Added converter

---
+++ SDConverter/converter.swift
@@ -0,0 +1,23 @@
+//
+//  converter.swift
+//  SDConverter
+//
+//  Created by Xongolab Shree on 29/08/19.
+//  Copyright © 2019 Xongolab Shree. All rights reserved.
+//
+
+import Foundation
+
+public final class converter {
+    
+    let name = "converter"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
